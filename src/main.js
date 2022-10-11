fb7dcec5e72f4cf872b1411991dc0200cb549823
scarlat-trainer <78.cristi@gmail.com>
2022-10-11 22:07:36 +0300

    add filter with lfo

--- src/main.js
+++ src/main.js
@@ -1,136 +1,187 @@
 import { initMidi, listMidiPorts, setSelectedMidiPort } from './midi.js';
 import {
-    triggerSynth,
-    resumeAudio,
-    handleChangeMasterVolume,
-    handleChangeVoiceAdsr,
-    handleChangeOsc1Level,
-    handleChangeOsc2Level,
-    handleChangeOsc2Detune,
-    handleChangeOsc1Type,
-    handleChangeOsc2Type
+  triggerSynth,
+  resumeAudio,
+  handleChangeMasterVolume,
+  handleChangeVoiceAdsr,
+  handleChangeOsc1Level,
+  handleChangeOsc2Level,
+  handleChangeOsc2Detune,
+  handleChangeOsc1Type,
+  handleChangeOsc2Type
 } from './synth.js';
 import { initADSRGraph, setADSRGraph } from './2dGraphs/adsrGraph.js';
 import '../public/style.css';
 
-
 var isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-    },
-    any: function () {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return (
+      navigator.userAgent.match(/IEMobile/i) ||
+      navigator.userAgent.match(/WPDesktop/i)
+    );
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  }
 };
 
 const onMidiEvent = (e) => {
-    const midiData = [];
-    for (var i = 0; i < e.data.length; i++) {
-        midiData.push(e.data[i]);
-    }
-    const event = midiData[0] >> 4;
-    const channel = midiData[0] & 0x0F;
+  const midiData = [];
+  for (var i = 0; i < e.data.length; i++) {
+    midiData.push(e.data[i]);
+  }
+  const event = midiData[0] >> 4;
+  const channel = midiData[0] & 0x0f;
 
-    triggerSynth({
-        event,
-        channel,
-        note: midiData[1],
-        velocity: midiData[2]
-    });
-}
+  triggerSynth({
+    event,
+    channel,
+    note: midiData[1],
+    velocity: midiData[2]
+  });
+};
+
+const keyToNote = {
+  z: 60,
+  s: 61,
+  x: 62,
+  d: 63,
+  c: 64,
+  v: 65,
+  g: 66,
+  b: 67,
+  h: 68,
+  n: 69,
+  j: 70,
+  m: 71
+};
+
+let keyBuff = null;
+
+const handleKeyNoteOn = (e) => {
+  if (e.key === keyBuff) return;
+  keyBuff = e.key;
+  console.log('on', e.key);
+  triggerSynth({
+    event: 9,
+    channel: 0,
+    note: keyToNote[e.key],
+    velocity: 120
+  });
+};
+
+const handleKeyNoteOff = (e) => {
+  console.log('off', e.key);
+  keyBuff = null;
+  triggerSynth({
+    event: 8,
+    channel: 0,
+    note: keyToNote[e.key],
+    velocity: 120
+  });
+};
 
 const startMidi = async () => {
-    await initMidi();
-    const midiPortsList = document.getElementById('midi-port-select');
-    const fragment = document.createDocumentFragment();
-    const { inputs, outputs } = listMidiPorts();
-    inputs.forEach(input => {
-        const portName = input[1].name;
-        const displaySelectedMidiPort = document.getElementById('dropdownMenuButtonMidiInput');
-        const listItem = document.createElement('button');
-        listItem.className = "dropdown-item";
-        listItem.innerText = portName;
-        listItem.onclick = () => {
-            setSelectedMidiPort(portName, onMidiEvent);
-            displaySelectedMidiPort.innerText = portName;
-            resumeAudio();
-        }
-        fragment.append(listItem);
-    });
+  await initMidi();
+  const midiPortsList = document.getElementById('midi-port-select');
+  const fragment = document.createDocumentFragment();
+  const { inputs, outputs } = listMidiPorts();
+  inputs.forEach((input) => {
+    const portName = input[1].name;
+    const displaySelectedMidiPort = document.getElementById(
+      'dropdownMenuButtonMidiInput'
+    );
+    const listItem = document.createElement('button');
+    listItem.className = 'dropdown-item';
+    listItem.innerText = portName;
+    listItem.onclick = () => {
+      setSelectedMidiPort(portName, onMidiEvent);
+      displaySelectedMidiPort.innerText = portName;
+      resumeAudio();
+    };
+    fragment.append(listItem);
+  });
 
-    midiPortsList.append(fragment);
-}
+  midiPortsList.append(fragment);
+};
 
 window.onload = async () => {
-    if (isMobile.any()) {
-        const mainContainer = document.getElementById('app');
-        mainContainer.style.display = 'none';
-        if (isMobile.iOS()){
-            alert('You are on iOS mobile.Please use Chrome browser only on desktop.');
-        }
-        else alert("Your are on mobile device.Please use Chrome browser only on desktop.");
-    }
-    else {
-        console.log('...loading MIDI ----> init synth graphs')
-        await startMidi();
-        initADSRGraph();
-    }
-}
+  if (isMobile.any()) {
+    const mainContainer = document.getElementById('app');
+    mainContainer.style.display = 'none';
+    if (isMobile.iOS()) {
+      alert('You are on iOS mobile.Please use Chrome browser only on desktop.');
+    } else
+      alert(
+        'Your are on mobile device.Please use Chrome browser only on desktop.'
+      );
+  } else {
+    console.log('...loading MIDI ----> init synth graphs');
+    await startMidi();
+    initADSRGraph();
+    addEventListener('keypress', handleKeyNoteOn, false);
+    addEventListener('keyup', handleKeyNoteOff);
+  }
+};
 
-
-const mainVolumeSlider = document.getElementById("main-volume-slider");
+const mainVolumeSlider = document.getElementById('main-volume-slider');
 mainVolumeSlider.oninput = handleChangeMasterVolume;
 
-const voiceAttack = document.getElementById("synth-adsr-attack");
+const voiceAttack = document.getElementById('synth-adsr-attack');
 voiceAttack.oninput = (e) => {
-    handleChangeVoiceAdsr(e.target.value, e.target.name);
-    setADSRGraph(e.target.value, e.target.name);
-}
+  handleChangeVoiceAdsr(e.target.value, e.target.name);
+  setADSRGraph(e.target.value, e.target.name);
+};
 
-const voiceDecay = document.getElementById("synth-adsr-decay");
+const voiceDecay = document.getElementById('synth-adsr-decay');
 voiceDecay.oninput = (e) => {
-    handleChangeVoiceAdsr(e.target.value, e.target.name);
-    setADSRGraph(e.target.value, e.target.name);
-}
+  handleChangeVoiceAdsr(e.target.value, e.target.name);
+  setADSRGraph(e.target.value, e.target.name);
+};
 
-const voiceSustain = document.getElementById("synth-adsr-sustain");
+const voiceSustain = document.getElementById('synth-adsr-sustain');
 voiceSustain.oninput = (e) => {
-    handleChangeVoiceAdsr(e.target.value, e.target.name);
-    setADSRGraph(e.target.value, e.target.name);
-}
+  handleChangeVoiceAdsr(e.target.value, e.target.name);
+  setADSRGraph(e.target.value, e.target.name);
+};
 
-const voiceRelease = document.getElementById("synth-adsr-release");
+const voiceRelease = document.getElementById('synth-adsr-release');
 voiceRelease.oninput = (e) => {
-    handleChangeVoiceAdsr(e.target.value, e.target.name);
-    setADSRGraph(e.target.value, e.target.name);
-}
+  handleChangeVoiceAdsr(e.target.value, e.target.name);
+  setADSRGraph(e.target.value, e.target.name);
+};
 
-const osc1Level = document.getElementById("synth-osc1-level");
+const osc1Level = document.getElementById('synth-osc1-level');
 osc1Level.oninput = (e) => handleChangeOsc1Level(e.target.value);
 
-const osc2Level = document.getElementById("synth-osc2-level");
+const osc2Level = document.getElementById('synth-osc2-level');
 osc2Level.oninput = (e) => handleChangeOsc2Level(e.target.value);
 
-const osc2Detune = document.getElementById("synth-osc2-detune");
+const osc2Detune = document.getElementById('synth-osc2-detune');
 osc2Detune.oninput = (e) => handleChangeOsc2Detune(e.target.value);
 
-const osc1Waveform = document.getElementById("osc1-waveform");
-osc1Waveform.onchange = (e) => handleChangeOsc1Type(e.target.id.split('').pop());
+const osc1Waveform = document.getElementById('osc1-waveform');
+osc1Waveform.onchange = (e) =>
+  handleChangeOsc1Type(e.target.id.split('').pop());
 
-const osc2Waveform = document.getElementById("osc2-waveform");
-osc2Waveform.onchange = (e) => handleChangeOsc2Type(e.target.id.split('').pop());
-
-
-
+const osc2Waveform = document.getElementById('osc2-waveform');
+osc2Waveform.onchange = (e) =>
+  handleChangeOsc2Type(e.target.id.split('').pop());
--- src/synth.js
+++ src/synth.js
@@ -1,16 +1,19 @@
 import Voice from './synth_modules/voice.js';
+import ADSR from './synth_modules/adsr.js';
 
-let audioContext = new (window.AudioContext || window.webkitAudioContext);
+let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 export const resumeAudio = () => {
-    audioContext.resume().then(() => {
-        console.log('Playback resumed successfully');
-    });
-}
+  audioContext.resume().then(() => {
+    console.log('Playback resumed successfully');
+  });
+};
 
 const mainVolume = audioContext.createGain();
 mainVolume.connect(audioContext.destination);
 mainVolume.gain.value = 0.5;
+
+const filter = audioContext.createBiquadFilter();
 
 const compressor = audioContext.createDynamicsCompressor();
 compressor.threshold.setValueAtTime(-30, audioContext.currentTime);
@@ -18,71 +21,108 @@ compressor.knee.setValueAtTime(40, audioContext.currentTime);
 compressor.ratio.setValueAtTime(10, audioContext.currentTime);
 compressor.attack.setValueAtTime(0, audioContext.currentTime);
 compressor.release.setValueAtTime(0.25, audioContext.currentTime);
-compressor.connect(mainVolume);
+compressor.connect(filter);
 
-const adsr = {attack: 0.1, decay: 0, sustain: 1, release: 0.5}
+const adsr = { attack: 0.1, decay: 0, sustain: 1, release: 0.5 };
 let osc1Level = 0.5;
 let osc2Level = 0.5;
 let osc2Detune = 0;
 
-const oscTypes = ["sine", "square", "sawtooth", "triangle"];
+const oscTypes = ['sine', 'square', 'sawtooth', 'triangle'];
 let osc1Type = 'sine';
 let osc2Type = 'sine';
 
 let voices = {};
 
+////////////////////////////////////////////////////
+const lfo = audioContext.createOscillator();
+const lfoGain = audioContext.createGain();
+lfoGain.gain.value = 1500;
+lfo.connect(lfoGain);
+lfo.frequency.value = 0;
+lfo.type = 'sine';
+lfo.start();
+
+filter.type = 'lowpass';
+filter.frequency.value = 2000;
+filter.connect(mainVolume);
+
+lfoGain.connect(filter.detune);
+const filterEnvelope = new ADSR(
+  audioContext,
+  filter.detune,
+  adsr.attack,
+  adsr.decay,
+  adsr.sustain,
+  adsr.release
+);
+
+////////////////////////////////////////////////////
+
 function noteToFreq(note) {
-    let a = 440;
-    return (a / 32) * (2 ** ((note - 9) / 12));
+  let a = 440;
+  return (a / 32) * 2 ** ((note - 9) / 12);
 }
 
 export const triggerSynth = (midiNoteEvent) => {
-    if (midiNoteEvent.event === 9) {
-        const voice = new Voice(audioContext, compressor, noteToFreq(midiNoteEvent.note), osc1Type, osc2Type);
-        voice.setEnvelope(adsr.attack, adsr.decay, adsr.sustain, adsr.release);
-        voice.setOsc1Level(osc1Level);
-        voice.setOsc1Type(osc1Type);
-        voice.setOsc2Level(osc2Level);
-        voice.setOsc2Type(osc2Type);
-        voice.setOsc2Detune(osc2Detune);
-        voice.triggerAttack();
-        voices[midiNoteEvent.note] = voice;
-    }
-    else if (midiNoteEvent.event === 8) {
-        //voices[midiNoteEvent.note].triggerRelease();
-        voices[midiNoteEvent.note].stop();
-        delete voices[midiNoteEvent.note];
-    }
-}
+  if (midiNoteEvent.event === 9) {
+    filterEnvelope.setEnvelope(
+      adsr.attack,
+      adsr.decay,
+      adsr.sustain * 1000,
+      adsr.release
+    );
+    const voice = new Voice(
+      audioContext,
+      compressor,
+      noteToFreq(midiNoteEvent.note),
+      osc1Type,
+      osc2Type
+    );
+    voice.setEnvelope(adsr.attack, adsr.decay, adsr.sustain, adsr.release);
+    voice.setOsc1Level(osc1Level);
+    voice.setOsc1Type(osc1Type);
+    voice.setOsc2Level(osc2Level);
+    voice.setOsc2Type(osc2Type);
+    voice.setOsc2Detune(osc2Detune);
+    voice.triggerAttack();
+    voices[midiNoteEvent.note] = voice;
+
+    filterEnvelope.triggerAttack();
+  } else if (midiNoteEvent.event === 8) {
+    filterEnvelope.triggerRelease();
+    voices[midiNoteEvent.note].stop();
+    delete voices[midiNoteEvent.note];
+  }
+};
 
 export const handleChangeMasterVolume = (e) => {
-    mainVolume.gain.value = e.target.value;
-}
+  mainVolume.gain.value = e.target.value;
+};
 
 export const handleChangeVoiceAdsr = (value, name) => {
-    adsr[name] = parseFloat(value);
-}
+  adsr[name] = parseFloat(value);
+};
 
 export const handleChangeOsc1Level = (value) => {
-    osc1Level = parseFloat(value);
-}
+  osc1Level = parseFloat(value);
+};
 
 export const handleChangeOsc2Level = (value) => {
-    osc2Level = parseFloat(value);
-}
+  osc2Level = parseFloat(value);
+};
 
 export const handleChangeOsc2Detune = (value) => {
-    console.log(parseFloat(value))
-    osc2Detune = parseFloat(value);
-}
+  console.log(parseFloat(value));
+  osc2Detune = parseFloat(value);
+};
 
 export const handleChangeOsc1Type = (value) => {
-    console.log(oscTypes[value - 1])
-    osc1Type = oscTypes[value - 1];
-}
+  console.log(oscTypes[value - 1]);
+  osc1Type = oscTypes[value - 1];
+};
 
 export const handleChangeOsc2Type = (value) => {
-    console.log(oscTypes[value - 1])
-    osc2Type = oscTypes[value - 1];
-}
-
+  console.log(oscTypes[value - 1]);
+  osc2Type = oscTypes[value - 1];
+};
